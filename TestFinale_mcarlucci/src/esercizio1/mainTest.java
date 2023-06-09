3aec4f9d6994d737b4e4b54fb7d7654b52c635ed
maurocarlu <maurocarlu@gmail.com>
2023-06-09 16:31:36 +0200

    Esercizio 2

--- TestFinale_mcarlucci/src/esercizio1/mainTest.java
+++ TestFinale_mcarlucci/src/esercizio1/mainTest.java
@@ -5,11 +5,12 @@ public class mainTest {
 	public static void main(String[] args) {
 		int[] arr = {547, 87, 1, 24, 4, 9, 54, 37, 26, 19};
 		double somma = 0;
+		double media = 0;
 		for(int i = 0; i < arr.length; i++) {
 			somma += arr[i];
 		}
-		double media = somma/arr.length;	
-		System.out.print(media);
+		media = somma/arr.length;	
+		System.out.print("Media: "+media);
 	}
 
 }
---
+++ TestFinale_mcarlucci/src/esercizio2/mainTest.java
@@ -0,0 +1,41 @@
+package esercizio2;
+
+import java.util.Scanner;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		int vEsame = 0;
+		int numEsami = 0;
+		double sommaVoti = 0;
+		double media = 0;
+		Scanner scanner = new Scanner(System.in);
+		do {
+			try {
+				System.out.print("Inserisci un voto oppure -1 per terminare l'inserimento: ");
+				vEsame = scanner.nextInt();
+				if(vEsame != -1) {
+					if(vEsame < 18 || vEsame > 30) {
+						System.out.println("valore inserito non corretto!");
+					} else {
+						sommaVoti += vEsame;
+						numEsami++;
+					}
+				}
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("Inserisci un numero!");
+				scanner.next(); //Evito il loop quando non viene inserito un numero.
+			}
+		}while(vEsame != -1);
+		scanner.close();
+		if(numEsami != 0) {	//Evito di stampare a video NaN (quando il denominatore è 0)
+			media = sommaVoti/numEsami;
+			System.out.println("Media dei voti: "+media);
+		} else {
+			System.out.println("Non sono stati inseriti voti dunque non calcolo la media.");
+		}
+		
+
+	}
+
+}
